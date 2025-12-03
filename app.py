6e6cfe7ff5e4cf8359d7291773a7b1bf73344bec
xplore-ops <gobinathxploreitcorp@gmail.com>
2025-12-03 14:37:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 def add():
     return "Hello"
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='13.37.228.209',port=5000)
+
 
 
 
